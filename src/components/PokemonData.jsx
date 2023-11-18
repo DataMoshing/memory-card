430e659488a9061c8fb3d64ded3a4ee2ad149dc1
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-17 20:44:25 -0700

    Created folder for components

---
+++ src/components/PokemonData.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonData() {
+    const [pokemonData, setPokemonData] = useState({})
+
+    // First fetch call for 10 Pokemon, name + url (array of objects) | nested API
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+            .then(response => response.json())
+            .then(function (allpokemon) {
+                console.log(allpokemon)
+                allpokemon.results.forEach(function (pokemon) {
+                    FetchPokemonData(pokemon)
+                })
+            })
+    }, [])
+    function FetchPokemonData(pokemon) {
+        let url = pokemon.url
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setPokemonData(data))
+    }
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <pre>{JSON.stringify(pokemonData, null, 2)}</pre>
+        </div>
+    )
+}
+
+// Second fetch call to get individual Pokemon data (type, number, etc)
