fc3b10595d2e63cf0205dc9b9151dcf8c36873a1
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-18 20:14:43 -0700

    Created file for pokemon data

---
+++ src/components/pokeData.js
@@ -0,0 +1,45 @@
+export default {
+    "success": true,
+    "data": {
+        "pokemon": [
+            {
+                "name": "bulbasaur",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif",
+            },
+            {
+
+                "name": "ivysaur",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif"
+
+            },
+            {
+                "name": "venasaur",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif"
+            },
+            {
+                "name": "charmander",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif"
+            },
+            {
+                "name": "charmeleon",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif"
+            },
+            {
+                "name": "charizard",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif"
+            },
+            {
+                "name": "squirtle",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif"
+            },
+            {
+                "name": "wartortle",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/8.gif"
+            },
+            {
+                "name": "blastoise",
+                "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/9.gif"
+            }
+        ]
+    }
+}
