a3562760d85f6eba54ca73dd4eaa1d94bc218065
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-20 16:33:30 -0700

    Pushing current code to repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import PokemonData from "./components/PokemonData"
+import Card from "./components/Card"
+import pokeData from "./components/pokeData"
+import "./styles/App.css"
 
-function App() {
+export default function App() {
+  let pokeNames = pokeData.data.pokemon
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+  }
+
+  // console.log(shuffleArray(pokeNames))
+  const cards = pokeNames.map(item => {
+    return (
+      <Card
+        img={item.img}
+        name={item.name}
+        key={item.id}
+      />
+    )
+  })
   return (
     <div>
-      <PokemonData />
+      <main>
+        {cards}
+      </main>
     </div>
   )
 }
 
-export default App
+
