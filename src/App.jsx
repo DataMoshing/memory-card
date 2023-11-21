c23fcec293aa40f017023493371e280ec8c4da4f
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-21 15:29:52 -0700

    Pushing current code towards repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
+// import { useEffect, useState } from "react";
 import Card from "./components/Card"
 import pokeData from "./components/pokeData"
 import "./styles/App.css"
 
-export default function App() {
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array
+}
+
+function App() {
   let pokeNames = pokeData.data.pokemon
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array
-  }
-
-  // console.log(shuffleArray(pokeNames))
-  const cards = pokeNames.map(item => {
+  const cards = pokeNames.map(card => {
     return (
       <Card
-        img={item.img}
-        name={item.name}
-        key={item.id}
+        img={card.img}
+        name={card.name}
+        key={card.id}
       />
     )
   })
+
   return (
     <div>
       <main>
-        {cards}
+        {shuffleArray(cards)}
       </main>
     </div>
   )
 }
 
+export { App }
 
